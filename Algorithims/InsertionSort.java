71c8d8f69fd9782d445ffd5cd00592a28f472f42
Samiul <samiul.taseen2002@gmail.com>
2025-03-12 22:11:19 +0000

    Quick Sort and Insertion Sort

---
+++ Algorithims/InsertionSort.java
@@ -0,0 +1,48 @@
+package Algorithims;
+
+import java.util.ArrayList;
+
+public class InsertionSort implements Algos {
+
+    @Override
+    public void SwapValues(ArrayList<Integer> list, int i, int j) {
+        int temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+
+    @Override
+    public int indexLowest() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Integer> SubArrays(ArrayList<Integer> Dataset) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> SubArrays() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> Subdeck(ArrayList<Integer> Dataset) {
+        return null;
+    }
+
+    public ArrayList<Integer> InsertionSort(ArrayList<Integer> dataset) {
+        int length = dataset.size();
+        for (int i = 1; i < length; i++) {
+            int key = dataset.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && dataset.get(j) > key) {
+                dataset.set(j + 1, dataset.get(j));
+                j--;
+            }
+            dataset.set(j + 1, key);
+        }
+        return dataset;
+    }
+}
---
+++ Algorithims/QuickSort.java
@@ -0,0 +1,56 @@
+package Algorithims;
+
+import java.util.ArrayList;
+
+public class QuickSort implements Algos {
+
+    @Override
+    public void SwapValues(ArrayList<Integer> list, int i, int j) {
+        int temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+
+    @Override
+    public int indexLowest() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<Integer> SubArrays(ArrayList<Integer> Dataset) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> SubArrays() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> Subdeck(ArrayList<Integer> Dataset) {
+        return null;
+    }
+
+    public ArrayList<Integer> QuickSort(ArrayList<Integer> dataset, int low, int high) {
+        if (low < high) {
+            int pivotIndex = Partition(dataset, low, high);
+            QuickSort(dataset, low, pivotIndex - 1);
+            QuickSort(dataset, pivotIndex + 1, high);
+        }
+        return dataset;
+    }
+
+    private int Partition(ArrayList<Integer> dataset, int low, int high) {
+        int pivot = dataset.get(high);
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (dataset.get(j) < pivot) {
+                i++;
+                SwapValues(dataset, i, j);
+            }
+        }
+        SwapValues(dataset, i + 1, high);
+        return i + 1;
+    }
+}
